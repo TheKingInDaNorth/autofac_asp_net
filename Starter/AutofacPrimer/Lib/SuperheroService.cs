a4cdd2bb5f5f34a881fd15efbe8b60909c1f5f2d
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2020-11-30 15:41:21 -0800

    11,30,20

---
+++ Starter/AutofacPrimer/Lib/SuperheroService.cs
@@ -0,0 +1,39 @@
+﻿using Lib.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace Lib
+{
+    public class SuperheroService
+    {
+        IAvengerRepository _avengerRepository;
+        ILogger _logger;
+
+        public SuperheroService(IAvengerRepository avengerRepository, ILogger logger)
+        {
+            _avengerRepository = avengerRepository;
+            _logger = logger;
+        }
+        public IEnumerable<Hero> GetAvengers()
+        {
+            _logger.Log("Calling SuperheroService.GetAvengers.");
+
+            var avengers = _avengerRepository.FetchAll();
+
+            _logger.Log("SuperheroService.GetAvengers called.");
+
+            return avengers;
+        }
+
+        public Hero GetAvenger(string name)
+        {
+            _logger.Log("Calling SuperheroService.GetAvenger('{0}').", name);
+
+            var avenger = _avengerRepository.Fetch(name);
+
+            _logger.Log("SuperheroService.GetAvenger('{0}') called.", name);
+
+            return avenger;
+        }
+    }
+}
