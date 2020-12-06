fe2b8c080c2fa405d985315f3ceb5d6d611abcc0
TheKingInDaNorth <tuan.le.vo19@gmail.com>
2020-12-06 12:37:20 -0800

    12,6,20

---
+++ Starter/Owin/OwinHost/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Lib;
+using Lib.Abstractions;
+using Owin;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+
+namespace OwinHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<AvengerRepository>().As<IAvengerRepository>().InstancePerRequest();
+            builder.RegisterType<Logger>().As<ILogger>().InstancePerRequest();
+            builder.RegisterType<LoggerMiddleware>().InstancePerRequest();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()).InstancePerRequest();
+
+            IContainer container = builder.Build();
+            
+            appBuilder
+                .UseAutofacWebApi(config)
+                .UseAutofacMiddleware(container)
+                .UseWebApi(config);
+        }
+    }
+}
